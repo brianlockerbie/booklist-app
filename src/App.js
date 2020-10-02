f5d9805ee88285340118b7f8645823ddfa97f37b
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-02 18:55:04 -0400

    Adding states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
-import './App.css';
+import "./App.css";
 
-const App () => {
+const App =  () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-  const [isbn, setIsbn] = useState(''),
-  
+  const [isbn, setIsbn] = useState('');
+  const [currentBookId, setCurrentBookId] = useState(null);
+  const [books, setBooks] = useState([
+    {
+      bookTitle: title,
+      bookAuthor: author,
+      bookIsbn: isbn,
+      bookId: uuidv4()
+    }
+  ])
+
   return (
     <div className="App">
       <h1>Welcome</h1>
